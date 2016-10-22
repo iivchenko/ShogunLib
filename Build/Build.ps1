7f95ddd54bcbca440e2af1215e2e9e237e0e44c3
shogun <iivchenko@live.com>
2016-10-22 17:31:06 +0100

    Provide build script
    Build solution
    Build package
    
    Issue #28

---
+++ Build/Build.ps1
@@ -0,0 +1,36 @@
+﻿$solution = "..\Src\ShogunLib.sln"
+$shogunLib = "..\Src\ShogunLib\ShogunLib.csproj"
+$nuget = ".\nuget.exe"
+$msb = "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+
+function Restore-Packages
+{
+    param($solution)
+    & $nuget restore $solution
+}
+
+function Build-Solution
+{
+    param($path, $configuration, $platform)
+    
+
+    & $msb $path /nologo /m /t:Rebuild /p:Configuration=$configuration /property:Platform=$platform
+}
+
+function Build-Package
+{
+    param($project)
+        
+    if (-not(Test-Path -Path "bin")) 
+    {
+        New-Item "bin" -ItemType Directory
+    }
+
+    & $nuget pack $project  -properties Configuration=Release -OutputDirectory "bin"
+}
+
+Restore-Packages -Solution $solution
+
+Build-Solution -Path $solution -Configuration Release -Platform "Any CPU"
+
+Build-Package -Project $shogunLib
--- Build/BuildPackage.bat
+++
@@ -1,1 +0,0 @@
-nuget.exe pack PackageProj\ShogunLib.nuspec
