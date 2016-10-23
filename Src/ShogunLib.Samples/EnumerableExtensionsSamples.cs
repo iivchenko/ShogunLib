351eda9d4cb48edcfae5efdd5f2d77a0df9468b0
shogun <iivchenko@live.com>
2016-10-23 13:17:48 +0100

    Add Enumerable Samples
    Issue #24

--- Src/ShogunLib.Samples/EnumerableExtensionsSamples.cs
+++ Src/ShogunLib.Samples/EnumerableExtensionsSamples.cs
@@ -4,12 +4,32 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+using ShogunLib.LINQ;
 
 namespace ShogunLib.Samples
 {
     [TestFixture]
     public sealed class EnumerableExtensionsSamples
     {
+        [Test]
+        public void Sample()
+        {
+            IEnumerable<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            IEnumerable<int> newList = new List<int>();
+
+            newList = list.ForEach(x => ++x);
+
+            list.ForEach(x => Console.Write("{0} ", x));
+
+            Console.WriteLine();
+
+            newList.ForEach(x => Console.Write("{0} ", x));
+        }
+        // Sample output
+        // 1 2 3 4 5
+        // 2 3 4 5 6
     }
 }
