c48bca4054cb05cd926eca4486d24641464bbc72
shogun <iivchenko@live.com>
2016-10-23 12:47:57 +0100

    Add Validate Samples
    Issue #24

--- Src/ShogunLib.Samples/ValidateSamples.cs
+++ Src/ShogunLib.Samples/ValidateSamples.cs
@@ -4,12 +4,30 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace ShogunLib.Samples
 {
     [TestFixture]
     public sealed class ValidateSamples
     {
+        [TestCase(null, null, null, "", 0)]
+        public void Validate(
+            object arg1,
+            Action arg2,
+            string arg3,
+            string arg4,
+            int arg5)
+        {
+            arg1.ValidateNull(nameof(arg1));
+            arg2.ValidateNull(nameof(arg2));
+            arg3.ValidateNull(nameof(arg3));
+
+            arg4.ValidateStringEmpty(nameof(arg4));
+
+            // arg5 can't be validated because it is not a ref type.
+        }
     }
 }
