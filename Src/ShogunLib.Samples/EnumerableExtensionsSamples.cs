d44625e5b6b0126bdf17107a4857da4641c6703a
shogun <iivchenko@live.com>
2016-10-23 12:32:33 +0100

    Add empty sample classes
    Issue #24
    
    [Rebase]Remove CA rules from ShogunLib.Samples
    
    Issue #24
    
    Revert "[Rebase]Remove CA rules from ShogunLib.Samples"
    
    This reverts commit 0f58691dfc73e31e06066a623bc73326a103debb.

---
+++ Src/ShogunLib.Samples/EnumerableExtensionsSamples.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2016 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using NUnit.Framework;
+
+namespace ShogunLib.Samples
+{
+    [TestFixture]
+    public sealed class EnumerableExtensionsSamples
+    {
+    }
+}
