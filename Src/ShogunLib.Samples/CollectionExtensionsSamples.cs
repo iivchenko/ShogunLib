b5a1caff6dafec02e6d147f204958de8803c10b0
shogun <iivchenko@live.com>
2016-10-30 23:53:22 +0200

    Add AddRange to Collection Extensions
    Issue #19

---
+++ Src/ShogunLib.Samples/CollectionExtensionsSamples.cs
@@ -0,0 +1,36 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2016 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using ShogunLib.LINQ;
+
+namespace ShogunLib.Samples
+{
+    [TestFixture(Category = Constants.Category)]
+    public sealed class CollectionExtensionsSamples
+    {
+        [Test]
+        public void Sample()
+        {
+            var collection = new Collection<int> { 1, 2 };
+            var list = new List<int> { 3, 4, 5 };
+
+            collection.ForEach(x => Console.Write("{0} ", x));
+
+            Console.WriteLine();
+
+            collection.AddRange(list);
+
+            collection.ForEach(x => Console.Write("{0} ", x));
+        }
+        // Sample output
+        // 1 2 
+        // 1 2 3 4 5 
+    }
+}
---
+++ Src/ShogunLib.Tests/LINQ/CollectionExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2016 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using ShogunLib.LINQ;
+
+namespace ShogunLib.Tests.LINQ
+{
+    [TestFixture]
+    public sealed class CollectionExtensionsTests
+    {
+        [Test]
+        public void AddRange_SourceIsNull_Throws()
+        {
+            ICollection<int> source = null;
+            var values = new List<int> { 1, 2, 3 };
+
+            Assert.Throws<ArgumentNullException>(() => source.AddRange(values));
+        }
+
+        [Test]
+        public void AddRange_ValuesIsNull_Throws()
+        {
+            var source = new Collection<int>();
+            IEnumerable<int> values = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.AddRange(values));
+        }
+
+        [Test]
+        public void AddRange_Test()
+        {
+            const int expectedCount = 3;
+
+            var source = new Collection<int>();
+            var values = new List<int> { 1, 2, 3 };
+
+            source.AddRange(values);
+
+            Assert.AreEqual(expectedCount, source.Count);
+        }
+    }
+}
