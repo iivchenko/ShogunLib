608c319150321ae58efb7a680091937dabcbc1a2
shogun <iivchenko@live.com>
2016-11-04 20:09:47 +0200

    Add empty class for Chain integration tests
    Issue #25

---
+++ Src/ShogunLib.Tests/Patterns/ChainOfResponsibility/Integration/ChainTests.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2016 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using NUnit.Framework;
+
+namespace ShogunLib.Tests.Patterns.ChainOfResponsibility.Integration
+{
+    [TestFixture]
+    public sealed class ChainTests
+    {
+    }
+}
