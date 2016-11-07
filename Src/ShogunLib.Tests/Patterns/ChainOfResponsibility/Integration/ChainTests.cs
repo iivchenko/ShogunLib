f4bac063ee6501695ce9f65c3892239135759127
shogun <iivchenko@live.com>
2016-11-07 17:20:51 +0200

    Add chain tests dummy
    Issue #25

--- Src/ShogunLib.Tests/Patterns/ChainOfResponsibility/Integration/ChainTests.cs
+++ Src/ShogunLib.Tests/Patterns/ChainOfResponsibility/Integration/ChainTests.cs
@@ -11,5 +11,360 @@ namespace ShogunLib.Tests.Patterns.ChainOfResponsibility.Integration
     [TestFixture]
     public sealed class ChainTests
     {
+        #region Void chain tests
+
+        #region No parameters
+
+        [Test]
+        public void VoidChain_NoParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void VoidChain_NoParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_NoParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_NoParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_NoParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region One parameter
+
+        [Test]
+        public void VoidChain_OneParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void VoidChain_OneParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_OneParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_OneParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_OneParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Two parameters
+
+        [Test]
+        public void VoidChain_TwoParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void VoidChain_TwoParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_TwoParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_TwoParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_TwoParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Three parameters
+
+        [Test]
+        public void VoidChain_ThreeParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void VoidChain_ThreeParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_ThreeParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_ThreeParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_ThreeParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Four parameters
+
+        [Test]
+        public void VoidChain_FourParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FourParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FourParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FourParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FourParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Five parameters
+
+        [Test]
+        public void VoidChain_FiveParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FiveParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChainFiveParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FiveParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void VoidChain_FiveParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Result chain tests
+
+        #region No parameters
+
+        [Test]
+        public void ResultChain_NoParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void ResultChain_NoParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_NoParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_NoParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_NoParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region One parameter
+
+        [Test]
+        public void ResultChain_OneParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void ResultChain_OneParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_OneParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_OneParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_OneParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Two parameters
+
+        [Test]
+        public void ResultChain_TwoParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void ResultChain_TwoParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_TwoParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_TwoParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_TwoParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Three parameters
+
+        [Test]
+        public void ResultChain_ThreeParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void ResultChain_ThreeParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_ThreeParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_ThreeParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_ThreeParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Four parameters
+
+        [Test]
+        public void ResultChain_FourParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FourParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FourParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FourParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FourParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #region Five parameters
+
+        [Test]
+        public void ResultChain_FiveParameters_NoLinks_DoNothing()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FiveParameters_WithLinks_FistLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChainFiveParameters_WithLinks_SecondLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FiveParameters_WithLinks_LastLinkDo()
+        {
+        }
+
+        [Test]
+        public void ResultChain_FiveParameters_WithLinks_DoNothing()
+        {
+        }
+
+        #endregion
+
+        #endregion
     }
 }
